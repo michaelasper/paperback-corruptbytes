b93ca82bbfde6f9558cb2a56963b07a93c12a7ee
Michael Asper <michaelksasper@gmail.com>
2026-08-08 18:47:44 -0700

    Fix MadaraDex metadata tag IDs

--- src/MadaraDex/pbconfig.ts
+++ src/MadaraDex/pbconfig.ts
@@ -4,7 +4,7 @@ export default {
   name: "MadaraDex",
   description:
     "A high-reliability MadaraDex extension with stable numeric IDs, automatic reader authorization, full discovery, and advanced search.",
-  version: "1.0.0-alpha.2",
+  version: "1.0.0-alpha.3",
   icon: "icon.png",
   language: "en",
   contentRating: ContentRating.ADULT,
